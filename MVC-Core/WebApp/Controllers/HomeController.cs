f7af18f67d029322cbdfcd14a74e4ff6e56784f7
Rojay Arinze <rojayarinze21@gmail.com>
2024-09-10 23:16:42 -0400

    add a home controller to the project

---
+++ MVC-Core/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+
+        public string Error()
+        {
+            return "Error LOL";
+        }
+    }
+}
