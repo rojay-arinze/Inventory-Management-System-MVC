413e87a10c5f456b5b8e9bf4a95b92d76189c501
Rojay Arinze <rojayarinze21@gmail.com>
2024-09-10 22:49:29 -0400

    #2 add a default url pattern so that the controller can map a particular request to an action method.

--- MVC-Core/WebApp/Program.cs
+++ MVC-Core/WebApp/Program.cs
@@ -4,4 +4,10 @@ var app = builder.Build();
 //#1 add routing middleware so that the app can handle routes
 app.UseRouting();
 
+//#2 add a default url pattern so that the controller can map a particular request to an action method.
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
 app.Run();
