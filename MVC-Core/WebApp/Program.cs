4507a030bde6cf6a22728fe4a5ded9d72f9c8d2a
Rojay Arinze <rojayarinze21@gmail.com>
2024-09-10 22:55:26 -0400

    update comments

--- MVC-Core/WebApp/Program.cs
+++ MVC-Core/WebApp/Program.cs
@@ -4,7 +4,10 @@ var app = builder.Build();
 //#1 add routing middleware so that the app can handle routes
 app.UseRouting();
 
-//#2 add a default url pattern so that the controller can map a particular request to an action method.
+/*
+#2 add a default url pattern so that the controller can map a particular request to an action method. 
+controller=Home: If no controller is specified, it maps to the Home controller
+action=Index: if no action is specified, it maps to the index action*/
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}"
