0d0444fbe003a9465d5e9d5af87118ccfe555973
Rojay Arinze <rojayarinze21@gmail.com>
2024-09-15 21:02:50 -0400

    adjust edit action in categories controller to respond appropriately to empty values.

--- MVC-Core/WebApp/Controllers/CategoriesController.cs
+++ MVC-Core/WebApp/Controllers/CategoriesController.cs
@@ -10,9 +10,17 @@ namespace WebApp.Controllers
         }
 
         /*Create an Action method to handle the request made when a user clicks on any item that has edit in the href attribute of an anchor tag*/
-        public IActionResult Edit(int id) //pass the id of the clicked item over to the edit method
+        public IActionResult Edit(int? id) //pass the id of the clicked item over to the edit method
         {
-            return new ContentResult { Content = id.ToString()};  //Populate the web page with the id of the clicked item.
+            if(id.HasValue)
+            {
+                return new ContentResult { Content = id.ToString() };  //Populate the web page with the id of the clicked item.
+
+            }
+            else
+            {
+                return new ContentResult { Content = "null content" };
+            }
         }
     }
 }
