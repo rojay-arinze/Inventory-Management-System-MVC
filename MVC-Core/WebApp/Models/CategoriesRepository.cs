17326f84583b424ee68cdb9e27139c19cf2caaa2
Rojay Arinze <rojayarinze21@gmail.com>
2024-09-16 20:36:32 -0400

    create a static in memory data repository to act as a database. Basically just a list of false data, and a few methods to perform crud operations on that list of false data called CategoriesRepository

---
+++ MVC-Core/WebApp/Models/CategoriesRepository.cs
@@ -0,0 +1,53 @@
+﻿namespace WebApp.Models
+{
+    public static class CategoriesRepository
+    {
+
+        private static List<Category> _categories = new List<Category>()
+        {
+            new Category{ CategoryId = 1, Name = "Beverage", Description= "Beverage"},
+            new Category {CategoryId = 2, Name = "Bakery", Description = "Bakery"},
+            new Category{CategoryId = 3, Name = "Meat", Description= "Meat"}
+        };
+
+        public static void AddCategory(Category category)
+        {
+            var maxId = _categories.Max(c => c.CategoryId);
+            category.CategoryId = maxId+1;
+            _categories.Add(category);
+        }
+
+        public static List<Category> GetCategories() => _categories;
+        
+        public static Category? GetCategoryById(int id)
+        {
+            var category = _categories.FirstOrDefault(x => x.CategoryId == id);
+            if(category != null)
+            {
+                return new Category { CategoryId = category.CategoryId, Name = category.Name, Description = category.Description };
+            }
+            return null;
+        }
+
+        public static void UpdateCategory(int id, Category category)
+        {
+            if(id != category.CategoryId) return;
+
+            var categoryToUpdate = GetCategoryById(id);
+            if(categoryToUpdate != null)
+            {
+                categoryToUpdate.Name = category.Name;
+                categoryToUpdate.Description = category.Description;
+            }
+        }
+
+        public static void DeleteCategory(int id)
+        {
+            var category = GetCategoryById(id);
+            if(category != null)
+            {
+                _categories.Remove(category);
+            }
+        }
+    }
+}
