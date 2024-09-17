f2d44441c610383cac7871e96652f024870460d5
Rojay Arinze <rojayarinze21@gmail.com>
2024-09-17 00:04:26 -0400

    add data annotation [Required] to ensure that a specific property is required, cannot be null

--- MVC-Core/WebApp/Models/Category.cs
+++ MVC-Core/WebApp/Models/Category.cs
@@ -1,9 +1,11 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
-
+        [Required]
         public string Name { get; set; } = string.Empty;
 
         public string Description { get; set; } = string.Empty ;
