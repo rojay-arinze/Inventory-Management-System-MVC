39f055283b0a56c59c5a2b55630d0b1f09710901
Rojay Arinze <rojayarinze21@gmail.com>
2024-09-17 10:43:04 -0400

    edit the category model, set the description string to nullable, otherwise it will be required when creating a category

--- MVC-Core/WebApp/Models/Category.cs
+++ MVC-Core/WebApp/Models/Category.cs
@@ -1,13 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApp.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
+
         [Required]
         public string Name { get; set; } = string.Empty;
-
-        public string Description { get; set; } = string.Empty ;
+        
+        public string? Description { get; set; } = string.Empty ;
     }
 }
