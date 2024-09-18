feb2b3fd71a79c4a7578e102c4cde9185ca51083
Rojay Arinze <rarinze1@ncstudents.niagaracollege.ca>
2024-09-17 23:13:47 -0400

    create edit httpget and httppost action method

--- MVC-Core/WebApp/Controllers/ProductsController.cs
+++ MVC-Core/WebApp/Controllers/ProductsController.cs
@@ -13,6 +13,7 @@ namespace WebApp.Controllers
 
         public IActionResult Add()
         {
+            ViewBag.Action = "add";
             ProductViewModel productsViewModel = new ProductViewModel(); //initialize a viewmodel to be passed to the add view
             productsViewModel.Categories = CategoriesRepository.GetCategories(); //populate the list of categories with categories
             return View(productsViewModel); //bind the viewmodel with the view and render the view
@@ -32,9 +33,27 @@ namespace WebApp.Controllers
 
         }
 
-        public IActionResult Edit(ProductViewModel productVM)
+        public IActionResult Edit(int id)
         {
+            ViewBag.Action = "edit";
+            var productVM = new ProductViewModel()
+            {
 
+                Categories = CategoriesRepository.GetCategories(),
+                Product = ProductsRepository.GetProductById(id) ?? new Product()
+            };
+            
+            return View(productVM);
+        }
+        [HttpPost]
+        public IActionResult Edit( ProductViewModel productVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var productToUpdate = productVM.Product;
+                ProductsRepository.UpdateProduct(productToUpdate.ProductId, productToUpdate);
+                return RedirectToAction(nameof(Index));
+            }
             productVM.Categories = CategoriesRepository.GetCategories();
             return View(productVM);
         }
