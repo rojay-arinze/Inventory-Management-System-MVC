828966a40d1f006c4178f12214c8239ff1ac5eec
Rojay Arinze <rarinze1@ncstudents.niagaracollege.ca>
2024-09-17 22:31:42 -0400

    create viewmodels directory under webapp project

---
+++ MVC-Core/WebApp/ViewModels/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿using WebApp.Models;
+/*This ViewModel is used to pass multiple pieces of data to the view. We needed to pass a product along with a list of categories.*/
+namespace WebApp.ViewModels
+{
+    public class ProductViewModel
+    {
+        public IEnumerable<Category> Categories { get; set; } = new List<Category>();
+
+        public Product Product { get; set; } = new Product();
+    }
+}
