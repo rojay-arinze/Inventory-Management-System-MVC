3b221154b5a3747d2757a516785716d374b4306e
Rojay Arinze <rarinze1@ncstudents.niagaracollege.ca>
2024-09-20 21:51:48 -0400

    git create salesviewmodel

---
+++ MVC-Core/WebApp/ViewModels/SalesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using WebApp.Models;
+
+namespace WebApp.ViewModels
+{
+    public class SalesViewModel
+    {
+        public int SelectedCategoryId { get; set; }
+
+        public List<Category> Categories { get; set; } = new List<Category>();
+    }
+}
