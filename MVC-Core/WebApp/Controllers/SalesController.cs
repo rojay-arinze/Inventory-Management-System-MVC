5ac83a44a94f2e4547e14ed3a113fb07a8c9d6db
Rojay Arinze <rarinze1@ncstudents.niagaracollege.ca>
2024-09-22 00:31:10 -0400

    create the sell Action method to handle the cashier trying to sell an item

--- MVC-Core/WebApp/Controllers/SalesController.cs
+++ MVC-Core/WebApp/Controllers/SalesController.cs
@@ -21,5 +21,17 @@ namespace WebApp.Controllers
                 var product = ProductsRepository.GetProductById(productId);
                 return PartialView("_Sales", product);
         }
+
+        public IActionResult Sell(SalesViewModel salesViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                //sell product
+            }
+            var product = ProductsRepository.GetProductById(salesViewModel.SelectedProductId);
+            salesViewModel.SelectedCategoryId = (product?.CategoryId == null)? 0:product.CategoryId.Value;
+            salesViewModel.Categories = CategoriesRepository.GetCategories();
+            return View("Index", salesViewModel);
+        }
     }
 }
